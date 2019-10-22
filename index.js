b0f12b5c582f0990a62767429c79c75c39f23b29
Caleb King <caleb.king536@gmail.com>
2019-10-22 10:39:33 -0600

    added filter function

--- index.js
+++ index.js
@@ -45,8 +45,6 @@ function average(numbers) {
   return sum / numbers.length;
 }
 
-console.log(average([-12, 6, 9]));
-
 // Create a function called repeat which takes two arguments:
 // The first argument should be an arbitrary function, fn
 // The second argument should be a number, n
@@ -65,5 +63,30 @@ const repeat = (fn, n) => {
 const hello = () => console.log("hello world");
 const goodbye = () => console.log("goodbye world");
 
-repeat(hello, 5);
-repeat(goodbye, 5);
+
+//Functions as arguments (2)
+
+function filter(arr, fn) {
+  const newArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (fn(arr[i]) === true) {
+      newArray.push(arr[i]);
+    }
+  }
+  return newArray;
+}
+
+// DO NOT EDIT BETWEEN THESE LINES, BUT DO READ THE CODE ----->
+// First we setup an array of strings we plan to filter:
+const myNames = ['Rich', 'Joe', 'Bhaumik', 'Ray'];
+
+// We use your `filter` function here, capturing a new array into `filteredNames`
+// comprised of names that only begin with 'R'
+const filteredNames = filter(myNames, function(name) {
+  // This is known as a "predicate function" - it's a function that 
+  // only returns a boolean
+  return name[0] === 'R';
+});
+
+console.log(filteredNames) // => ['Rich', 'Ray']
+// <---- DO NOT EDIT BETWEEN THESE LINES
