39222ee75d041a45be7eff5be23768a4c1c40d99
Caleb King <caleb.king536@gmail.com>
2019-10-22 20:38:41 -0600

    Completed Nested For Loop challenge

--- index.js
+++ index.js
@@ -150,3 +150,23 @@ function logNumSteps(move, index) {
 }
 
 console.table(filteredMoves);
+
+
+//Nesting For Loops - Freecodecamp
+
+function multiplyAll(arr) {
+  var product = 1;
+  // Only change code below this line
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+      product *= arr[i][j];
+    }
+  }
+
+  // Only change code above this line
+  return product;
+}
+
+// Modify values below to test your code
+multiplyAll([[1,2],[3,4],[5,6,7]]);
